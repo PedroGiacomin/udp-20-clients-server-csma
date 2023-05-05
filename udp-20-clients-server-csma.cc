5e3528eb161e2af444856d1977616df3db32eba8
giacomin-p <pedrogiacomin2706@gmail.com>
2023-05-05 18:02:02 -0300

    Arquivos pcap adicionados

--- udp-20-clients-server-csma.cc
+++ udp-20-clients-server-csma.cc
@@ -15,6 +15,7 @@
 #include "ns3/internet-module.h"
 
 #include "ns3/netanim-module.h"
+#include "ns3/mobility-module.h"
 
 using namespace ns3;
 
@@ -32,11 +33,9 @@ main (int argc, char *argv[]){
 
     // --- CRIACAO DOS NODES --- //    
     NS_LOG_INFO ("Create nodes.");
-    NodeContainer serverNode;
-    NodeContainer clientNodes;
-    serverNode.Create(1);
-    clientNodes.Create(numClients);
-    NodeContainer allNodes(serverNode, clientNodes);
+
+    NodeContainer allNodes;
+    allNodes.Create(numClients + 1);
 
     // --- INTERNET --- // 
     NS_LOG_INFO("Install internet protocols stack.");
@@ -61,9 +60,11 @@ main (int argc, char *argv[]){
 
     // --- APLICACOES --- //
     uint16_t serverPort = 9;
-    NS_LOG_INFO ("Create UdpServerEcho application on node 1.");
+    NS_LOG_INFO ("Create UdpServerEcho application on node 0.");
     UdpEchoServerHelper serverHelper (serverPort);
-    ApplicationContainer serverApp = serverHelper.Install (serverNode.Get(0));
+    ApplicationContainer serverApp = serverHelper.Install (allNodes.Get(0));
+    serverApp.Start (Seconds (1.0));
+    serverApp.Stop (Seconds (10.0));
 
     NS_LOG_INFO ("Create UdpClientEcho applications.");
     uint32_t packetSize = 1024;
@@ -71,27 +72,36 @@ main (int argc, char *argv[]){
     Time interPacketInterval = Seconds (1.);
 
     //Uma aplicacao por cliente
+    UdpEchoClientHelper clientHelper(serverAddress, serverPort); 
+    clientHelper.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
+    clientHelper.SetAttribute ("Interval", TimeValue (interPacketInterval));
+    clientHelper.SetAttribute ("PacketSize", UintegerValue (packetSize));
     std::vector<ApplicationContainer> clientApp(numClients);
     for(uint32_t i=0; i<clientApp.size (); ++i){
-        UdpEchoClientHelper clientHelper(serverAddress, serverPort); 
-        clientHelper.SetAttribute ("MaxPackets", UintegerValue (maxPacketCount));
-        clientHelper.SetAttribute ("Interval", TimeValue (interPacketInterval));
-        clientHelper.SetAttribute ("PacketSize", UintegerValue (packetSize));
-        clientApp[i] = clientHelper.Install(clientNodes.Get(i)); //instala app no cliente i
+        clientApp[i] = clientHelper.Install(allNodes.Get(i + 1)); //instala app no node i+1
         clientApp[i].Start(Seconds(2.0));
         clientApp[i].Stop(Seconds(10.0));
     }
+
+    // TRACING
+    AsciiTraceHelper ascii;
+    csma.EnableAsciiAll (ascii.CreateFileStream ("tracing/udp-20-clients-server-csma/udp-20-clients-server-csma.tr"));
+    csma.EnablePcapAll ("tracing/udp-20-clients-server-csma/udp-20-clients-server-csma.tr", false);
 
     // --- NETANIM --- //
     NS_LOG_INFO("Set animation.");
     AnimationInterface anim ("udp-20-clients-server-csma-anim.xml");
 
-    anim.SetConstantPosition(serverNode.Get(0), 300, 300); //server = node 0
-    uint32_t x = 0, y = 0;
+    MobilityHelper mobility;
+    mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+    mobility.Install(allNodes);
+
+    uint32_t centerX = 30, centerY = 30, x = 0, y = 0;
+    anim.SetConstantPosition(allNodes.Get(0), centerX, centerY); //server = node 0
     for(uint32_t i=0; i<numClients; ++i){
-        x = 100 + 40*i;
-        y = (i <= 9) ? (400) : (200);
-        anim.SetConstantPosition(clientNodes.Get(i), x, y); 
+        x = centerX + 10;
+        y = centerY + 10*i;
+        anim.SetConstantPosition(allNodes.Get(i+1), x, y); 
     }
 
     // --- EXECUCAO --- //
